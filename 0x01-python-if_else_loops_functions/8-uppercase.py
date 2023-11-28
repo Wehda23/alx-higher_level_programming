50229c72eed12ef2c6940aaf2bdadb64cc95ae76
Waheed Khaled <waheedkhaled95@gmail.com>
2023-11-28 22:37:11 +0000

    hot fix spaces

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def uppercase(str):
     for index, character in enumerate(str):
+
         if ord(character) >= 97 and ord(character) <= 122:
             character = chr(ord(character) - 32)
-        print("{}".format(character), end= "")
-        if index == len(str) - 1:
-            print("")
+
+        print("{}".format(character), end="")
+
+    print("")
