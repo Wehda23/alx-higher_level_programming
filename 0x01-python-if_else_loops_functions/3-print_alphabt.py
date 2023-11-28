efbc1da23eb14ec97fd8ddc6bc3c0d877cb0ebad
Waheed Khaled <waheedkhaled95@gmail.com>
2023-11-28 14:19:53 +0000

    test

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-def func(number):
+def fu(number):
     return True if number not in (101, 113) else False
-
-print(''.join(['{}'.format(chr(i)) for i in range(97, 123) if func(i)]), end='')
+print(''.join(['{}'.format(chr(i)) for i in range(97, 123) if fu(i)]), end='')
