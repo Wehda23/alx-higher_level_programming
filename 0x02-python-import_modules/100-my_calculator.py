e9f0ef12c1a1ef701b3f8d9bb9d2487764046a3d
Waheed Khaled <waheedkhaled95@gmail.com>
2023-12-01 02:56:28 +0000

    done

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,22 +1,36 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
 import sys
+from calculator_1 import add, sub, mul, div
+
+
+def calculator():
+
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+
+    a = int(sys.argv[1])
+    operator = sys.argv[2]
+    b = int(sys.argv[3])
+
+    result = None
+    if operator == '+':
+        result = add(a, b)
+    elif operator == '-':
+        result = sub(a, b)
+    elif operator == '*':
+        result = mul(a, b)
+    elif operator == '/':
+        if b == 0:
+            print("Error: Division by zero is not allowed")
+            sys.exit(1)
+        result = div(a, b)
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
+    print(f"{a} {operator} {b} = {result}")
 
 
 if __name__ == "__main__":
-
-    if len(sys.argv) - 1 != 3:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
-    elif sys.argv[2] not in ["-", "+", "*", "/"]:
-        print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
-    else:
-        a = int(sys.argv[1])
-        b = int(sys.argv[3])
-
-        match sys.argv[2]:
-            case "+": print("{} + {} = {}".format(a, b, add(a, b)))
-            case "-": print("{} - {} = {}".format(a, b, sub(a, b)))
-            case "*": print("{} * {} = {}".format(a, b, mul(a, b)))
-            case "/": print("{} / {} = {}".format(a, b, div(a, b)))
+    calculator()
