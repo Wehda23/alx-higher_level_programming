1331ff2b054477cca03086e04b146e51ff4b8eb8
Waheed Khaled <waheedkhaled95@gmail.com>
2023-12-04 13:56:48 +0000

    task 6 done

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for array in matrix:
+        for number ,element in enumerate(array, start=1):
+            if number == len(array):
+                print("{}".format(element), end="")
+            else:
+                print("{} ".format(element), end='')
+        print("")
