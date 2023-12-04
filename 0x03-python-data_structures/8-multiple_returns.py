0c52955bf0232c4188a22976a71d8c18e3b8fa47
Waheed Khaled <waheedkhaled95@gmail.com>
2023-12-04 14:14:04 +0000

    task 8 done

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    first = sentence[0] or None
+    first = None
+    if len(sentence) > 0:
+        first = sentence[0]
+
     return (len(sentence), first)
