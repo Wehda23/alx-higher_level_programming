bbb305ed2f920ed1c76486c276a42719a2234fbd
Waheed Khaled <waheedkhaled95@gmail.com>
2023-12-04 13:59:52 +0000

    task 6 done

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     for array in matrix:
         for number, element in enumerate(array, start=1):
             if number == len(array):
-                print("{}".format(element), end="")
+                print("{:d}".format(element), end="")
             else:
-                print("{} ".format(element), end='')
+                print("{:d} ".format(element), end='')
         print("")
