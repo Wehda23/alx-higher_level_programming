db494d84b78cc18b2e7e92136c7c5adb4d2868d4
Waheed Khaled <waheedkhaled95@gmail.com>
2023-12-06 13:38:23 +0000

    task 1 done

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    return [ number if number != search else replace for number in my_list]
+    return [number if number != search else replace for number in my_list]
