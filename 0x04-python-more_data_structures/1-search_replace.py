052dc9d3b885560c7cce3570391889ce1987cd50
Waheed Khaled <waheedkhaled95@gmail.com>
2023-12-06 13:37:28 +0000

    edited logic

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = []
-    for index, number in enumerate(my_list):
-        if index + 1 == search:
-            new_list.append(replace)
-        else:
-            new_list.append(number)
-    return new_list
+    return [ number if number != search else replace for number in my_list]
