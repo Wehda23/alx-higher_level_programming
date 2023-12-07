cede1694dce5d5d518456024d1ebd8932cb477c3
Waheed Khaled <waheedkhaled95@gmail.com>
2023-12-07 00:06:50 +0000

    task 14 done

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return list(map(lambda array: [num**2 for num in array], matrix))
+    return list(map(lambda array: list(map(lambda n: n**2, array)), matrix))
