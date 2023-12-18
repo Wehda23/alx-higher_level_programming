c2fc69d6936cbcb4d5c88fdbc9d51329c5e00d36
Waheed Khaled <waheedkhaled95@gmail.com>
2023-12-18 23:55:23 +0000

    task 8 done

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+
+def safe_function(fct, *args):
+    try:
+        a, b = args
+        result = fct(a, b)
+        return result
+    except Exception as e:
+        print("Exception: {}".format(e), file=sys.stderr)
+        return None
+
