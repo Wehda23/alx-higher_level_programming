8341b7610c591b409a6d85ccc36b6195cb258c2f
Waheed Khaled <waheedkhaled95@gmail.com>
2023-12-18 23:56:18 +0000

    Pycodestyle

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import sys
 
+
 def safe_function(fct, *args):
     try:
         a, b = args
@@ -9,4 +10,3 @@ def safe_function(fct, *args):
     except Exception as e:
         print("Exception: {}".format(e), file=sys.stderr)
         return None
-
