f67d16684390eda1dcc96f590f919bb72914cd9b
Waheed Khaled <waheedkhaled95@gmail.com>
2023-12-18 23:11:31 +0000

    task 1 done

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-def safe_print_list_integers(my_list=[], x=0):
-    try:
-        for number in range(x):
-            print(my_list[number], end="")
-        print()
-    except:
-        pass
+def safe_print_integer(value) -> bool:
+    if isinstance(value, int):
+        print("{:d}".format(value))
+        return True
+    return False
