f2cf471a26f9c5672df0584b027fbc444c0b4fb0
Waheed Khaled <waheedkhaled95@gmail.com>
2023-12-19 22:54:00 +0000

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This is a task made purposely for task 1
+"""
+
+
+class Square:
+    """Class made for purpose to serve task 1"""
+    def __init__(self, size):
+        self._size = size
