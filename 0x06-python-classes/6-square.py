428f5ebac73faa755c8791c9808dd9807bcb49f7
Waheed Khaled <waheedkhaled95@gmail.com>
2023-12-19 23:45:49 +0000

    task 6 done

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -6,7 +6,7 @@ This is a task made purposely for task 4
 
 class Square:
     """Class made for purpose to serve task 4"""
-    def __init__(self, size=0):
+    def __init__(self, size=0, position=(0, 0)):
         """Adding Size to the class instance as private instance
         Args:
          - size: Size of a square.
@@ -18,7 +18,11 @@ class Square:
         if not (size >= 0):
             raise ValueError("size must be >= 0")
 
+        if not all([isinstance(pa, int) and (pa >= 0) for pa in position]):
+            raise TypeError("position must be a tuple of 2 positive integers")
+
         self.__size = size
+        self.__position = position
 
     def area(self):
         """
@@ -40,12 +44,26 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = value
 
+    @property
+    def position(self):
+        """Property to retrieve position"""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """Setter method for changing value of position"""
+        if not all([(isinstance(pa, int)) and (pa >= 0) for pa in value]):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
     def my_print(self):
         """ Method used to print the square in # or nothing """
         if self.__size == 0:
             print()
         else:
-            for row in range(self.__size):
-                for column in range(self.__size):
-                    print("#", end="")
+            for i in range(self.__position[1]):
                 print()
+            for row in range(self.__size):
+                for column in range(self.__position[0]):
+                    print(" ", end="")
+                print("#" * self.__size)
