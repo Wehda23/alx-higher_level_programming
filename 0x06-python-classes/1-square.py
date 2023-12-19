a730d9ae12109bc6dc8f2d2565f4a086eaaca728
Waheed Khaled <waheedkhaled95@gmail.com>
2023-12-19 22:55:49 +0000

    Task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -7,4 +7,8 @@ This is a task made purposely for task 1
 class Square:
     """Class made for purpose to serve task 1"""
     def __init__(self, size):
+        """Adding Size to the class instance as private instance
+        Args:
+         - size: Size of a square.
+        """
         self._size = size
