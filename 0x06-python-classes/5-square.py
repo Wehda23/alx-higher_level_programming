f30c0b3a606e6be4da66f32dea2a228c13486932
Waheed Khaled <waheedkhaled95@gmail.com>
2023-12-19 23:24:58 +0000

    task 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""
+This is a task made purposely for task 4
+"""
+
+
+class Square:
+    """Class made for purpose to serve task 4"""
+    def __init__(self, size=0):
+        """Adding Size to the class instance as private instance
+        Args:
+         - size: Size of a square.
+        """
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        if not (size >= 0):
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+        """
+        Public method used to return the area of the square based on size
+        """
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """Proptery to retrieve size """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter function the set a new value for size"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if not (value >= 0):
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        """ Method used to print the square in # or nothing """
+        if self.__size == 0:
+            print()
+        else:
+            for row in range(self.__size):
+                for column in range(self.__size):
+                    print("#",end="")
+                print()
