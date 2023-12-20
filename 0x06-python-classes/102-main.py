8ea4e2aad2cfb17f18f2e3dcca90ba2fbda30965
Waheed Khaled <waheedkhaled95@gmail.com>
2023-12-20 01:18:32 +0000

    task done

--- 0x06-python-classes/102-main.py
+++ 0x06-python-classes/102-main.py
@@ -5,7 +5,7 @@ s_5 = Square(5)
 s_6 = Square(6)
 
 if s_5 < s_6:
-        print("Square 5 < Square 6")
+    print("Square 5 < Square 6")
 if s_5 <= s_6:
     print("Square 5 <= Square 6")
 if s_5 == s_6:
@@ -15,4 +15,4 @@ if s_5 != s_6:
 if s_5 > s_6:
     print("Square 5 > Square 6")
 if s_5 >= s_6:
-print("Square 5 >= Square 6")
+    print("Square 5 >= Square 6")
