c730bfb59f87404ed3e787c44d1f32af58968386
Waheed Khaled <waheedkhaled95@gmail.com>
2024-01-08 23:27:22 +0000

    Task 3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+File that contains a function called 
+is_kind_of_class
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    Instance of class or inherited from
+    """
+    return isinstance(obj, a_class)
