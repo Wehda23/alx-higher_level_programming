9d9c6b7f3bbbebf05f62f68d36249999f10f3096
Waheed Khaled <waheedkhaled95@gmail.com>
2024-01-08 23:56:07 +0000

    Task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+File contains class Called Geomtry
+"""
+
+
+class BaseGeometry:
+    """
+    Class For gemotric representations
+    """
+    def area(self):
+        """Method that should calculate Area of Geometry"""
+        raise Exception("area() is not implemented")
