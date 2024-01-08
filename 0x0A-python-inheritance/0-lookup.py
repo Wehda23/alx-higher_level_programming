61c87d4ca220fe9e81d3b6501d969e5f7ddda2c3
Waheed Khaled <waheedkhaled95@gmail.com>
2024-01-08 21:53:16 +0000

    task 0 done

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
+"""
+Contains a function that returns attributes and methods\
+        of an object.
+"""
+
+
 def lookup(obj):
+    """Function that returns objects's attributes and methods"""
     return dir(obj)
