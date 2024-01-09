90dad6a27c3030f4db219f3c27faa1d2bb6091e4
Waheed Khaled <waheedkhaled95@gmail.com>
2024-01-09 01:17:55 +0000

    task 9 doen

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+File contains class Called Geomtry
+"""
+
+
+class BaseGeometry:
+    """
+    Class For gemotric representations
+    """
+    def area(self):
+        """Method that should calculate Area of Geometry"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Method to validate value"""
+
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value < 1:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """
+    Class representeds a 2D Geometry
+    """
+    def __init__(self, width, height):
+        """Initiating new instance"""
+        # Validate width
+        self.integer_validator("width", width)
+        self.__width = width
+        # Validate height
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """Returns the area of the rectangle"""
+        return self.__height * self.__width
+
+    def __str__(self):
+        """String of object"""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
