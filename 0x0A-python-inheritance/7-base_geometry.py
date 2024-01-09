f18c3cabd5f98d38ad5fa4f2077b905676299973
Waheed Khaled <waheedkhaled95@gmail.com>
2024-01-09 00:47:56 +0000

    Dude some thing is not right

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -20,7 +20,7 @@ class BaseGeometry:
             - name: Should of a python <str> type
             - value: Should be of python type <int>
         """
-        if not isinstance(value, int) or isinstance(value, bool):
+        if not isinstance(value, int):
             raise TypeError(f"{name} must be an integer")
         if value < 1:
             raise ValueError(f"{name} must be greater than 0")
