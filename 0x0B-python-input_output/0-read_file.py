28a0dbbc04ed6cbb7a05e5d8ee7e442957d1e963
Waheed Khaled <waheedkhaled95@gmail.com>
2024-01-09 22:11:42 +0000

    Done

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -8,4 +8,4 @@ File contains function that reads files
 def read_file(filename: str = ""):
     """Reads a file and returns its content as string."""
     with open(filename, encoding="utf-8", mode="r") as file:
-        print(file.read())
+        print(file.read(), end="")
