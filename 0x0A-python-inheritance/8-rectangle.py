1ce111099abb365d069ef64c843e51d7cfc30e9d
Waheed Khaled <waheedkhaled95@gmail.com>
2024-01-09 00:42:11 +0000

    Task 7 edited, task 8 done

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -24,3 +24,22 @@ class BaseGeometry:
             raise TypeError(f"{name} must be an integer")
         if value < 1:
             raise ValueError(f"{name} must be greater than 0")
+
+
+class Rectangle(BaseGeometry):
+    """
+    Two Dimensional Rectangular Shape Class
+    """
+    def __init__(self, width: int, height: int):
+        """
+        Intializing a new class instance
+
+        Args:
+            - width : Width of the rectangle as in integers
+            - height: Height of the rectangle as in integers
+        """
+        # Validate integer
+        self.integer_validator('width', width)
+        self.integer_validator('height', height)
+        self.__width = width
+        self.__height = height
