3d3bfd1d31a1198d015372d7d0096d489d470ac5
Waheed Khaled <waheedkhaled95@gmail.com>
2024-01-10 01:01:16 +0000

    Task 11 done

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+"""
+File contains class Called Student
+"""
+
+
+class Student:
+    """
+    Class Represents A Student
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Class Initiation
+        """
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None) -> dict:
+        """Method Retrieves Dictionary Representation"""
+        data: dict = vars(self)
+
+        if attrs and isinstance(attrs, list):
+            data: dict = {
+                    key: value for key, value in data.items()
+                    if key in attrs
+            }
+
+        return data
+
+    def reload_from_json(self, json):
+        """
+        Method that updates class attributes
+        """
+        for key, value in json.items():
+            self.key = value
