6f54dde10e2b41cb4f2094d5dfa9d1f1510f681d
Waheed Khaled <waheedkhaled95@gmail.com>
2024-01-10 00:39:35 +0000

    task 10 done

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+"""
+File contains class Called Student
+"""
+
+
+class Student:
+    """
+    Class Represents A Student
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Class Initiation
+        """
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None) -> dict:
+        """Method Retrieves Dictionary Representation"""
+        data: dict = vars(self)
+
+        if attrs:
+            data: dict = {
+                    key: value for key, value in data.items()
+                    if key in attrs
+            }
+
+        return data
