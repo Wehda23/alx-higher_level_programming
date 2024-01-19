ba3f8002e62bd6bff33295ffe5a3b6b4ded23b53
Waheed Khaled <waheedkhaled95@gmail.com>
2024-01-19 19:25:52 +0200

    test

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -64,7 +64,7 @@ class Rectangle(Base):
             Nothing, just Raises an Error
         """
         # Check type
-        if not isinstance(value, int):
+        if type(value) != int:
             raise TypeError(f"{name} must be an integer")
 
     @staticmethod
