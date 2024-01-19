ed6882e5ebd916965751fc3ffa082120a2929e88
Waheed Khaled <waheedkhaled95@gmail.com>
2024-01-19 18:18:41 +0200

    testing

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" File that contains class Base """
+
+
+class Base:
+    """
+    The goal of it is to manage id attribute in all my
+    future classes and to avoid duplicating the same code
+    by (extrension, same bugs)
+    """
+    __nb_objects = 0
+
+    def __init__(self, id= None):
+        """
+        Initiating class instance
+
+        Args:
+            - id: Id of the instance
+        """
+
+        if id:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
