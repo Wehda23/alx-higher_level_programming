5f0753e3ed118283d974cd43deabcd53d69a387f
Waheed Khaled <waheedkhaled95@gmail.com>
2024-01-19 18:36:07 +0200

    test

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -6,3 +6,179 @@ from models.base import Base
 
 class Rectangle(Base):
     """Class that represents a two dimensional Rectangle"""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        Class instance initiation attributes.
+
+        Args:
+            - width: Width of the rectangle.
+            - height: Height of the rectangle.
+            - x: Postion according to the x-axis (default: 0)
+            - y: Position according to the y-axis (default: 0)
+        """
+        # Initiate Super
+        super().__init__(id)
+        self.height = height
+        self.width = width
+        self.x = x
+        self.y = y
+    
+    @staticmethod
+    def validate_positive_integer(value: int, name: str) -> None:
+        """
+        Method used to validate value as positive integer
+
+        Args:
+            - value: integer input
+            - name: variable name
+
+        Returns:
+            - Nothing, Just raises an error
+        """
+        # Validate integer type
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+
+        # Check value of the input
+        if value <= 0:
+            raise ValueError(f"{name} must be > 0")
+
+    @staticmethod
+    def validate_integer(value: int, name: str) -> None:
+        """
+        Method used to validate if the value is an integer
+
+        Args:
+            - value: integer input
+            - name: variable name
+
+        Returns:
+            Nothing, just Raises an Error
+        """
+        # Check type
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+
+    @staticmethod
+    def validate_coordinate(value: int, name: str) -> None:
+        """
+        Static Method used to validate coordinates
+
+        Args:
+            - value: Integer value input
+            - name: name of variable
+        """
+        # Check if it is equal or more than 0
+        if value < 0:
+            raise ValueError(f"{name} must be >= 0")
+
+    @property
+    def width(self) -> int:
+        """
+        Property method to get value to width
+
+        Returns:
+            - width of the rectangle as Python Integer Value <int>
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value: int) -> None:
+        """
+        Property Setter method for new value for width
+
+        Args:
+            - value: New value for width of rectangle
+
+        Returns:
+            - Nothing.
+        """
+        # Initiate variable name
+        variable_name: str = "width"
+
+        # Validate parameters
+        self.validate_positive_integer(value, variable_name)
+
+        # Set new value
+        self.__width: int = value
+
+    @property
+    def height(self) -> int:
+        """
+        Property method to get value of height
+
+        Returns:
+            - height of the rectangle as Python Integer value <int>
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value) -> None:
+        """
+        Property Setter method used to set new value to height
+
+        Args:
+           - value: New value of height
+        """
+        # Initate variable name
+        variable_name: str = 'height'
+
+        # Validate value
+        self.validate_positive_integer(value, variable_name)
+
+        # Set new value to height
+        self.__height: int = value
+
+    @property
+    def x(self) -> int:
+        """
+        Property method used to return X coordinate of rectangle
+
+        Returns:
+            - X-Coordinate of rectangle as python integer <int>
+        """
+        return self.__x
+
+    @x.setter
+    def x(self, value: int) -> None:
+        """
+        Property Setter method used to set new value to x-axis
+
+        Args:
+            - value: New x-coordinate for rectangle
+        """
+        # Initiate variable name
+        variable_name: str = "x"
+        # Validate value
+        self.validate_integer(value, variable_name)
+        self.validate_coordinate(value, variable_name)
+        # Set New value
+        self.__x: int = value
+
+    @property
+    def y(self) -> int:
+        """
+        Property Method used to return Y coordinate of rectangle
+
+        Returns:
+            - Y-Coordinate of rectangle as Python Integer <int>
+        """
+        return self.__y
+
+    @y.setter
+    def y(self, value: int) -> None:
+        """
+        Property Setter method used to set new value to y-axis
+
+        Args:
+            - value: New y-coordinate for rectangle
+        """
+        # Initiate variable name
+        variable_name: str = 'y'
+        # Validate value
+        self.validate_integer(value, variable_name)
+        self.validate_coordinate(value, variable_name)
+
+        # Set New Value
+        self.__y = value
