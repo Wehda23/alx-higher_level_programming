53ffd5b1f6a93ce33e43a06f60b2f81ec0ff22d9
Waheed Khaled <waheedkhaled95@gmail.com>
2024-03-11 20:29:55 +0200

    Task 2 Done

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,3 +1,10 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
-console.log(argv.join('\n'));
+
+if (argv.length == 3) {
+  console.log('Argument found');
+} else if (argv.length > 3) {
+  console.log('Arguments found');
+} else {
+  console.log('No argument');
+}
