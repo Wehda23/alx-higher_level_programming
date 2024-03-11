aec989c96dd1c6abbae324606943caa76fde114d
Waheed Khaled <waheedkhaled95@gmail.com>
2024-03-11 21:25:39 +0200

    Up

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+function factorial (number) {
+  if (number === 1) {
+    return 1;
+  }
+  return number * factorial(number - 1);
+}
+
+const input = parseInt(process.argv[2]);
+
+console.log(factorial(input));
