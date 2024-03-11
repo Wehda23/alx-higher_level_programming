0502a5ad7e2829e193a9c93ba640532ab462f7ab
Waheed Khaled <waheedkhaled95@gmail.com>
2024-03-11 20:58:01 +0200

    Task 5 done

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
-const { argv } = require('node:process');
-const argument = parseInt(argv[2]);
-if (argument) {
-  console.log(`My number: ${argument}`);
-} else {
+
+if (isNaN(process.argv[2])) {
   console.log('Not a number');
+} else {
+  console.log(`My number: ${parseInt(process.argv[2])}`);
 }
