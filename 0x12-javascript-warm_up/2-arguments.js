d1b194795b18f443805fd926750835740c24403e
Waheed Khaled <waheedkhaled95@gmail.com>
2024-03-11 20:31:50 +0200

    Adjusted == to ===

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
 
-if (argv.length == 3) {
+if (argv.length === 3) {
   console.log('Argument found');
 } else if (argv.length > 3) {
   console.log('Arguments found');
