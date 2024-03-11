f04e0bb56ca7854d8bdf038bc754db400b0f816c
Waheed Khaled <waheedkhaled95@gmail.com>
2024-03-11 21:55:29 +0200

    Fixing

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+const [, , ...argv] = process.argv;
+
+if (argv.length < 2) {
+  console.log(0);
+} else {
+  let stack = [];
+
+  for (let i = 0; i < argv.length; i++) {
+    if (stack.length < 2) {
+      stack = [parseInt(argv[i]), ...stack];
+    } else {
+      if (stack[0] < parseInt(argv[i])) {
+        stack[0] = parseInt(argv[i]);
+      }
+    }
+
+    if (stack[0] > stack[1]) {
+      swap(stack);
+    }
+  }
+
+  if (stack[0] > stack[1]) {
+    swap(stack);
+  }
+  console.log(stack[0]);
+}
+
+function swap(stack) {
+  let temp = stack[0];
+  stack[0] = stack[1];
+  stack[1] = temp;
+}
