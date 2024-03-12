deaf7d8e4f3a79e26de8fafc2c0a803068a94f91
Waheed Khaled <waheedkhaled95@gmail.com>
2024-03-12 22:34:46 +0200

    hot fix for task 12

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const { dict } = require('./101-data');
+const { dict } = require("./101-data");
 const userIds = [];
 
 Object.values(dict).forEach((user) => {
@@ -20,5 +20,4 @@ const people = userIds.reduce((acc, userId) => {
   return acc;
 }, {});
 
-console.log(dict);
 console.log(people);
