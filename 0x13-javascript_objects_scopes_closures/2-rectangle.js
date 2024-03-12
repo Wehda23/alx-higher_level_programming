a6a1684a0503c3409019e0882e0d34be736f3a56
Waheed Khaled <waheedkhaled95@gmail.com>
2024-03-12 21:07:20 +0200

    Test

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,16 +2,10 @@
 
 class Rectangle {
   constructor (w, h) {
-    this.setWidth(w > 0 && h > 0 ? w : undefined);
-    this.setHeight(h > 0 && w > 0 ? h : undefined);
-  }
-
-  setWidth (width) {
-    this.width = width;
-  }
-
-  setHeight (height) {
-    this.height = height;
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
   }
 }
 
