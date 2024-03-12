704c16a762f8d80a54f58b213a22fc05dcc92c68
Waheed Khaled <waheedkhaled95@gmail.com>
2024-03-12 22:16:12 +0200

    Strings..

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const { list } = require("./100-data");
+const { list } = require('./100-data');
 const multipliedList = list.map((item, index) => item * index);
 console.log(list);
 console.log(multipliedList);
