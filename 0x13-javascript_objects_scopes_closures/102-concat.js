e8445d986a1dea78e548a41833f2f097d275920d
Waheed Khaled <waheedkhaled95@gmail.com>
2024-03-12 22:44:59 +0200

    Done

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
-const fs = require('fs');
+const fs = require("fs");
 
 // Check if correct number of arguments are provided
 if (process.argv.length !== 5) {
-  console.error('Usage: ./102-concat.js <file1> <file2> <destination>');
+  console.error("Usage: ./102-concat.js <file1> <file2> <destination>");
   process.exit(1);
 }
 
@@ -12,14 +12,14 @@ if (process.argv.length !== 5) {
 const [, , file1, file2, destination] = process.argv;
 
 // Read content of file1
-fs.readFile(file1, 'utf8', (err, data1) => {
+fs.readFile(file1, "utf8", (err, data1) => {
   if (err) {
     console.error(`Error reading ${file1}: ${err.message}`);
     process.exit(1);
   }
 
   // Read content of file2
-  fs.readFile(file2, 'utf8', (err, data2) => {
+  fs.readFile(file2, "utf8", (err, data2) => {
     if (err) {
       console.error(`Error reading ${file2}: ${err.message}`);
       process.exit(1);
