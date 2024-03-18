bfe7d2bb9ae1f1ee08207947df780024700c849d
Waheed Khaled <waheedkhaled95@gmail.com>
2024-03-18 03:39:30 +0200

    Hot fix

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -14,9 +14,11 @@ def main() -> None:
         charset="utf8",
     )
     cur = conn.cursor()
-    query = (
-        """SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY states.id ASC"""
-    )
+    query = """
+    SELECT *
+    FROM states
+    WHERE name LIKE BINARY '{}' ORDER BY states.id ASC
+    """
     query = query.format(argv[4])
     cur.execute(query)
     query_rows = cur.fetchall()
