142197306e39c78ab0b29cc57c27df97f603ed8f
Waheed Khaled <waheedkhaled95@gmail.com>
2024-03-29 03:20:44 +0200

    Done

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Python script that retrieves the 10 most recent commits
+from a given repository by a specific user using the GitHub API.
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    repo_name = sys.argv[1]
+    owner_name = sys.argv[2]
+
+    url = f"https://api.github.com/repos/{owner_name}/{repo_name}/commits"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        commits = response.json()
+        for commit in commits[:10]:
+            sha = commit.get('sha')
+            author_name = commit.get('commit').get('author').get('name')
+            print(f"{sha}: {author_name}")
+    else:
+        print("Error: Unable to fetch commits.")
