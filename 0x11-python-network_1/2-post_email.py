5c017f1c011631f6c8ed2e019f3a33d1f6b88b33
Waheed Khaled <waheedkhaled95@gmail.com>
2024-03-29 02:39:56 +0200

    Task 3-7 done

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -12,8 +12,8 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
-    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+    data = urllib.parse.urlencode({"email": email}).encode("utf-8")
     req = urllib.request.Request(url, data)
     with urllib.request.urlopen(req) as response:
         body = response.read()
-        print(body.decode('utf-8'))
+        print(body.decode("utf-8"))
