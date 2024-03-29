e117b5628a265d0406708e784e73527b63a62e0c
Waheed Khaled <waheedkhaled95@gmail.com>
2024-03-29 03:23:15 +0200

    Done

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -10,15 +10,10 @@ import sys
 if __name__ == "__main__":
     repo_name = sys.argv[1]
     owner_name = sys.argv[2]
-
     url = f"https://api.github.com/repos/{owner_name}/{repo_name}/commits"
     response = requests.get(url)
-
-    if response.status_code == 200:
-        commits = response.json()
-        for commit in commits[:10]:
-            sha = commit.get('sha')
-            author_name = commit.get('commit').get('author').get('name')
-            print(f"{sha}: {author_name}")
-    else:
-        print("Error: Unable to fetch commits.")
+    commits = response.json()
+    for commit in commits[:10]:
+        sha = commit.get('sha')
+        author_name = commit.get('commit').get('author').get('name')
+        print(f"{sha}: {author_name}")
