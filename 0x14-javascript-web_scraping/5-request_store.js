4f85e9c72f31cfe1bddfe49bfd073c618d2737e8
Waheed Khaled <waheedkhaled95@gmail.com>
2024-04-23 16:02:27 +0200

    Added Fs

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 // Script that display the status code of a GET request
 const request = require('request');
+const fs = require('fs');
 const url = process.argv[2];
 const path = process.argv[3];
 
