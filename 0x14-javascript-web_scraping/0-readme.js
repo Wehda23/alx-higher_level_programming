0ef63e9153e723fb2aee9abdb4556cc4f1acdb56
Waheed Khaled <waheedkhaled95@gmail.com>
2024-04-23 09:50:40 +0000

    Test

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,16 +1,16 @@
 // Dependencies
-const fs = require("fs");
+const fs = require('fs');
 
 // Grab Arguments and File name
 const args = process.argv;
 const fileName = args[2] ? args[2] : null;
 
 try {
-    // Read content of a file
-    const content = fs.readFileSync(fileName, 'utf8');
-    // Print the content
-    console.log(content);
+  // Read content of a file
+  const content = fs.readFileSync(fileName, 'utf8');
+  // Print the content
+  console.log(content);
 } catch (error) {
-    // In case of error print error
-    console.error(error);
+  // In case of error print error
+  console.error(error);
 }
