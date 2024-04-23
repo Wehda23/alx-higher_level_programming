8775db14409746f984b5532d2474748610f7d9a5
Waheed Khaled <waheedkhaled95@gmail.com>
2024-04-23 07:26:01 -0700

    Task Done and ready for ALX Checkers

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -3,24 +3,22 @@
 const request = require('request');
 const url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
 
-
 request(url, function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
     const data = JSON.parse(body).characters;
-    for (let i = 0; i < data.length; i++)
-    {
-    const character = data[i];
-    request(character, function (error, response, body) {
-    if (error) {
-    console.log(error);
-    } else {
-    const name = JSON.parse(body).name;
-    console.log(name);
-    }
-    }
-    )
+    for (let i = 0; i < data.length; i++) {
+      const character = data[i];
+      request(character, function (error, response, body) {
+        if (error) {
+          console.log(error);
+        } else {
+          const name = JSON.parse(body).name;
+          console.log(name);
+        }
+      }
+      );
     }
   }
 });
