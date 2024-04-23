11bf1723914814cdb15be4726e7387d6c09dd78a
Waheed Khaled <waheedkhaled95@gmail.com>
2024-04-23 16:19:27 +0200

    Test

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+// Script that display the status code of a GET request
+const request = require('request');
+const url = `https://swapi-api.alx-tools.com/api/film/${process.argv[2]}`;
+
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const data = JSON.parse(body).characters;
+    for (let i = 0; i < data.length; i++)
+    {
+    const character = data[1];
+    request(character, function (error, response, body) {
+    if (error) {
+    console.log(error);
+    } else {
+    const name = JSON.parse(body).name;
+    console.log(name);
+    }
+    }
+    )
+    }
+  }
+});
