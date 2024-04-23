9ba5486980d30a99c4007d8a0f46123f1ca6503d
Waheed Khaled <waheedkhaled95@gmail.com>
2024-04-23 15:51:42 +0200

    asd:

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -10,14 +10,14 @@ request(url, function (error, response, body) {
   } else {
     const movies = JSON.parse(body).results;
     for (let i = 0; i < movies.length; i++) {
-	    const characters = movies[i].characters;
-	    for (let j = 0; j < characters.length; j++) {
-		    const character = characters[j];
-		    const characterId = character.split('/')[5];
-		    if (characterId === '18') {
-          count++;
-		    }
-	    }
+    const characters = movies[i].characters;
+    for (let j = 0; j < characters.length; j++) {
+    const character = characters[j];
+    const characterId = character.split('/')[5];
+    if (characterId === '18') {
+      count++;
+    }
+    }
     }
   }
 
